83acfb2db266f6251a403179786528b2a2442e7f
Maarten Gregoire <maarten.gregoire@cegeka.com>
2016-11-04 13:41:26 +0100

    basic project

---
+++ src/main/java/JenkinsTryout/JenkinsTryout.java
@@ -0,0 +1,11 @@
+package JenkinsTryout;
+
+/**
+ * Created by maarteng on 4/11/2016.
+ */
+public class JenkinsTryout {
+
+    public void main (String[] args){
+        System.out.println("HELLO!");
+    }
+}
